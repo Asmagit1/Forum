96b3b7989447cbf119f467666dd03672fb8fe67b
Asma <asma.zgarni@esprit.tn>
2025-05-08 18:15:19 +0200

    Initialisation du projet

---
+++ BackendForum/src/test/java/tn/esprit/backendforum/BackendForumApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.backendforum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendForumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
